02e1feff4592c71acefb3132f3fec8592e5e37b5
Mike Shilovski <ShMike6491@gmail.com>
2021-06-02 20:05:06 +0300

    navigation component

--- app/src/main/java/com/materialkotlin/MainActivity.kt
+++ app/src/main/java/com/materialkotlin/MainActivity.kt
@@ -2,16 +2,10 @@ package com.materialkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.materialkotlin.features.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if(savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .add(R.id.container, HomeFragment())
-                .commitNow()
-        }
     }
 }
