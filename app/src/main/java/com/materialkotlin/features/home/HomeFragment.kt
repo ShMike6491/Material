330704cda487b68d69e4d6cd847d20ac37834a5f
Mike Shilovski <ShMike6491@gmail.com>
2021-06-08 20:57:12 +0300

    spans

--- app/src/main/java/com/materialkotlin/features/home/HomeFragment.kt
+++ app/src/main/java/com/materialkotlin/features/home/HomeFragment.kt
@@ -1,8 +1,14 @@
 package com.materialkotlin.features.home
 
+import android.graphics.Color
+import android.graphics.Typeface.BOLD
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.BulletSpan
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
 import android.view.View
-import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import coil.api.load
@@ -32,6 +38,44 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
 
         viewModel.getLiveData().observe(viewLifecycleOwner, { applyChanges(it) })
         viewModel.requestDailyImage()
+
+        renderText()
+    }
+
+    private fun renderText() {
+        val spannable = SpannableString("My text \nbullet one\nbullet two")
+
+        fun rnd(): Int = (0..255).random()
+        val color = Color.argb(255, rnd(), rnd(), rnd())
+
+        spannable.setSpan(
+            BulletSpan(40, color),
+            /* начало элемента списка */ 9, /* конец элемента списка */ 18,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        spannable.setSpan(
+            BulletSpan(40, color),
+            /* начало элемента списка */ 20, /* конец элемента списка */ spannable.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        spannable.setSpan(
+            ForegroundColorSpan(color),
+            0, 7,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        spannable.setSpan(
+            StyleSpan(BOLD),
+            /* начало элемента списка */ 1, /* конец элемента списка */ spannable.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+        binding.tvSpan.text = spannable
+
+/*
+        // передаём span
+        textView.setText(spannableString, BufferType.SPANNABLE)
+        // получаем текст и кастим его в span, потому что ранее передали туда span
+        val spannableText = textView.text as Spannable
+*/
     }
 
     private fun applyChanges(data: NasaResponse) {
